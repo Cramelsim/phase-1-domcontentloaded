dafb09065fd1955e03554ce3f4a44be0f38a039e
MELODY SIMWA <simwamelody@gmail.com>
2024-12-09 12:38:46 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const targetElement = document.querySelector("#text");
+    if (targetElement) {
+      targetElement.textContent = "This is really cool!";
+    }
+  });
+  
